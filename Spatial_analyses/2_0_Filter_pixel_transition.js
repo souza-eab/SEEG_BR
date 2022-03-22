0c1fd3f85ed7b544fe7a4048eea3cedb2af1ad51
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2022-03-21 21:37:54 -0300

    Update 2_0_Filter_pixel_transition.js

--- Spatial_analyses/2_0_Filter_pixel_transition.js
+++ Spatial_analyses/2_0_Filter_pixel_transition.js
@@ -1,14 +1,22 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////// GOALS: EXCLUDE ISOLATED PIXELS AND REDUCE NOISE MAPBIOMAS (eg. col 6.0) ///////////////////////////////////
+//////////  Coordination: Barbara Zimbres, Julia Shimbo, and Ane Alencar /////////////////////////////////////////////////////
+//////////  Developed by: IPAM, SEEG and Climate Observatory ////////////////////////////////////////////////////////////////
+////////// Citing: Zimbres et al.,2022.  ///////////////////////////////////////////////////////////////////////////////////
+/////////  Processing time <2h> in Google Earth Engine ////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// SCRIPT SCRIPT TO FILTER DEFORESTATION AND REGENERATION MASKS TO EXCLUDE ISOLATED PIXELS AND REDUCE NOISE "MAPBIOMAS (eg. col 6.0)" 
-// For any issue/bug, please write to <edriano.souza@ipam.org.br>; <dhemerson.costa@ipam.org.br>; <barbara.zimbres@ipam.org.br>
-// Developed by: IPAM, SEEG and OC
-// Citing: SEEG/Observatório do Clima and IPAM
-// Processing time <1-2h> in Google Earth Engine
+// @. UPDATE HISTORIC //
+// 1: 
+// 1.1: 
+// 1.1: 
+// @. ~~~~~~~~~~~~~~ // 
 
-// Definition of the functions used
-// Set Asset
-// Apply set
+/* @. Set user parameters */// eg.
+
+// set directory for the output file
+var dir_output = 'projects/mapbiomas-workspace/SEEG/2021/Col9/';
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -28,7 +36,7 @@ var collection2multiband = function (collection) {
     return ee.Image(multiBand);
 };
 
-//Função que aplica o Majority Filter (square Kernel)
+// Create Function that applies the Majority Filter (square_Kernel)
 var PostClassification = function(image) {
 
     this.init = function(image) {
@@ -81,27 +89,31 @@ var PostClassification = function(image) {
 };
 
 
-//Agora aplicamos as funções definidas anteriormente
- 
-//Feature da região de interesse, no caso, todos os biomas do Brasil
+//////////////////////////////////////////////////////////////////////////////////////////
+// Now we apply the functions defined previously
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Feature of the region of interest, in this case, all biomes in Brazil
 var Bioma = ee.FeatureCollection("users/SEEGMapBiomas/bioma_1milhao_uf2015_250mil_IBGE_geo_v4_revisao_pampa_lagoas");
 
-//Parametros do filtro espacial
+// Specify spatial filter parameters
 var filterParams = [
    {classValue: 1, maxSize: 5}, // o tamanho maximo que o mapbiomas está usando é 5, valor definido em reunião (kernel de 5 pixels)
    {classValue: 0, maxSize: 5}
 ];
 
-// Verificar se à partir daqui considera apenas 1990 ou de 1985
-//A partir daqui trabalhamos apenas a partir de 1990, que é o período inicial apresentado pelo Inventário Nacional
+
+// From here on, we work from 1990, which is the initial period of official data presented by the National Inventory (QCN,2020) 
 var anos = ['1990','1991','1992','1993','1994','1995','1996','1997','1998','1999','2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020'];
 
+// Creat list years
 var eeAnos = ee.List(anos);
 
-/////Chama o asset da máscara de REGENERAÇÃO exportado a partir do script 1
+///// Input the Asset 'REGENERATION' mask asset exported from step 1.0
 var inputImage_regen = ee.Image('projects/mapbiomas-workspace/SEEG/2021/Col9/regenSEEGc6'); // alterar para o asset salvo por vocês no script anterior
 
-//Aplica as funções
+// Apply functions
 var result_regen = eeAnos.map(function(ano){
   filterParams;
   PostClassification;
@@ -111,26 +123,27 @@ var result_regen = eeAnos.map(function(ano){
     return(filtered2.int8());
 });
 
-// Salva o resultado como uma imagem multi-bandas
+// Save the result as a multi-band (ImageCollection)
 result_regen = collection2multiband(ee.ImageCollection.fromImages(result_regen));
 print(result_regen);
 
+// Export 
 Export.image.toAsset({
     "image": result_regen.uint8(),
     "description": 'regenSEEGc6_filter_certo',
-    "assetId": 'projects/mapbiomas-workspace/SEEG/2021/Col9/regen_SEEGc6_filter', //insere o endereço para onde vai exportar a máscara filtrada
+    "assetId": 'projects/mapbiomas-workspace/SEEG/2021/Col9/regen_SEEGc6_filter', // Enter the address and name 'project/seeg/col9/v1'of the Asset to be exported
     "scale": 30,
     "pyramidingPolicy": {
         '.default': 'mode'
     },
     "maxPixels": 1e13,
-    "region": Bioma.geometry().convexHull() //altera para a região utilizada por vocês
+    "region": Bioma.geometry().convexHull() // If desired, change here to the name of the desired region in Brazil
 });
 
-/////Chama o asset da máscara de DESMATAMENTO exportado a partir do script 1
-var inputImage_desm = ee.Image('projects/mapbiomas-workspace/SEEG/2021/Col9/desmSEEGc6'); // alterar para o asset salvo por vocês no script anterior
+///// Input the Asset 'DESFORESTATION' mask asset exported from step 1.0  
+var inputImage_desm = ee.Image('projects/mapbiomas-workspace/SEEG/2021/Col9/desmSEEGc6'); // change to the asset saved by you in the previous script
 
-//Aplica as funções
+// Apply function 
 var result_desm = eeAnos.map(function(ano){
   filterParams;
   PostClassification;
@@ -140,18 +153,19 @@ var result_desm = eeAnos.map(function(ano){
     return(filtered2.int8());
 });
 
-// Salva o resultado como uma imagem multi-bandas
+// Save the result as a multi-band (ImageCollection)
 result_desm = collection2multiband(ee.ImageCollection.fromImages(result_desm));
 print(result_desm);
 
+// Export
 Export.image.toAsset({
     "image": result_desm.uint8(),
     "description": 'desmSEEGc6_filter_certo',
-    "assetId": 'projects/mapbiomas-workspace/SEEG/2021/Col9/desmSEEGc6_filter', //insere o endereço para onde vai exportar a máscara filtrada
+    "assetId": 'projects/mapbiomas-workspace/SEEG/2021/Col9/desmSEEGc6_filter', // Enter the address and name 'project/seeg/col9/v1'of the Asset to be exported
     "scale": 30,
     "pyramidingPolicy": {
         '.default': 'mode'
     },
     "maxPixels": 1e13,
-    "region": Bioma.geometry().convexHull() //altera para a região utilizada por vocês
+    "region": Bioma.geometry().convexHull() //If desired, change here to the name of the desired region in Brazil
 });

44ab87990d7c2429752dd7b9d0de5c9a73ae9a45
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2022-08-25 11:15:54 -0300

    Update 3.1_Export_to_Asset.js

--- 1._Spatial_analyses/3.1_Export_to_Asset.js
+++ 1._Spatial_analyses/3.1_Export_to_Asset.js
@@ -24,17 +24,18 @@ var dir_output = 'projects/ee-seeg-brazil/assets/collection_10/v1/';
 ///////////////////////////////////////////////////////////////////////
 
 // Export parameters
-var gfolder = 'TEMP';                // google drive folder 
-var assetId = 'projects/mapbiomas-workspace/SEEG/2022/Col10'; // asset link
+//var gfolder = 'TEMP';                // google drive folder 
+//var assetId = 'projects/mapbiomas-workspace/SEEG/2022/Col10'; // asset link
 
 // Define the data path
 var dir = 'projects/ee-seeg-brazil/assets/collection_10/v1/3_0_Transition_maps';
            
 // Define filename prefix
-var prefix = '3_1_SEEG_Transitions_';
+var prefix = 'SEEG_Transitions_';
 
 // Define pairs of years to be processed
-var listYears = ['1985_1986','1986_1987', '1987_1988', '1988_1989', '1989_1990', '1990_1991', '1991_1992', '1992_1993', '1993_1994', '1994_1995', '1995_1996', 
+var listYears = ['1985_1986','1986_1987', '1987_1988', '1988_1989', '1989_1990', '1990_1991', '1991_1992',
+                 '1992_1993', '1993_1994', '1994_1995', '1995_1996', 
                  '1996_1997', '1997_1998', '1998_1999', '1999_2000', '2000_2001', '2001_2002', '2002_2003',
                  '2003_2004', '2004_2005', '2005_2006', '2006_2007', '2007_2008', '2008_2009', '2009_2010', 
                  '2010_2011', '2011_2012', '2012_2013', '2013_2014', '2014_2015', '2015_2016', '2016_2017',
@@ -70,8 +71,8 @@ print(trans);
 // Export as a GEE asset 
   Export.image.toAsset({
     'image': trans,
-    'description': prefix + 'stacked',
-    'assetId': dir_output + prefix + 'stacked',
+    'description': '3_1_'+ prefix + 'stacked',
+    'assetId':  dir_output + '3_1_'+ prefix + 'stacked',
     'pyramidingPolicy': {
         '.default': 'mode'
     },

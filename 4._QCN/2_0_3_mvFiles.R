e5bce2381197eacd852ded5099a4cf3968cf3bfe
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2022-09-13 16:12:33 -0300

    Create 2_0_3_mvFiles.R

---
+++ 4._QCN/2_0_3_mvFiles.R
@@ -0,0 +1,15 @@
+## create metadata for each tile
+## dhemerson.costa@ipam.org.br
+
+library (filesstrings)
+
+##  list pattern
+pat <- c('cagb', 'cbgb', 'cdw', 'clitter', 'total', 'qcnclass')
+
+## mv files
+for (i in 1:length(pat)) {
+  files <- list.files ('./to_up', pattern=paste0(pat[i], '.tif'), full.names=TRUE)
+  for (j in 1:length(files)) {
+    file.move(files[j], paste0('./to_up/', pat[i]))
+  }
+}

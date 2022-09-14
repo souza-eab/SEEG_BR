f4e890855e72e69dc2e0708e9ee6175c73d757e7
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2022-09-14 17:44:40 -0300

    Update 2_0_1_CreateQCN_tiles.R

--- 4._QCN/2_0_1_CreateQCN_tiles.R
+++ 4._QCN/2_0_1_CreateQCN_tiles.R
@@ -1,4 +1,4 @@
-## rasterize QCN vectors 
+### rasterize QCN vectors 
 ## edriano.souza@ipam.org.br or dhemerson.costa@ipam.org.br
 
 
@@ -10,13 +10,19 @@ library(raster)
 
 ## user parameters
 ## path with QCN vectors 
-root <- "../vector/qcn_class"
+#root <- "../vector/qcn_class"
+root <-"C:/Users/edriano.souza/GitHub/2022_2_QCN_rectify_v2/data/shp/biomes_v0"
 
 ## define filename of vector to be rasterized
 vector  <- st_make_valid(read_sf(dsn= root, layer= "classes_biomes"))
 
 ## define tiles file
-tiles <- read_sf(dsn= '../vector/qcn_veg_pret', layer='tiles_biomes')
+#tiles <- read_sf(dsn= '../vector/qcn_veg_pret', layer='tiles_biomes')
+tiles <- read_sf(dsn= "C:/Users/edriano.souza/GitHub/2022_2_QCN_rectify_v2/data/shp/biomes_v0/qcn_veg_pret", 
+                 layer="tiles_biomes")
+
+
+## define tiles file
 
 ####### end of user parameters, don't change after this line ########
 ## rasterize fieldnames per tile and stack bands 
@@ -27,7 +33,7 @@ for (i in 1:nrow(tiles)) {
   ## clip vector for the tile i
   vec_i <- st_intersection(vector, tile_i)
   ## create mask
-  mask <- raster(crs=projection(vec_i), ext= extent(vec_i)); res(mask) = 0.00025 0.002245
+  mask <- raster(crs=projection(vec_i), ext= extent(vec_i)); res(mask) = 0.00025 
   ## convert to stars
   mask <- st_as_stars(mask)
   
@@ -37,15 +43,24 @@ for (i in 1:nrow(tiles)) {
   r_cdw  <- st_rasterize(vec_i['cdw'], template= mask)
   r_clitter <- st_rasterize(vec_i['clitter'], template= mask)
   r_total <- st_rasterize(vec_i['ctotal4inv'], template= mask)
-  r_class <- st_rasterize(vec_i['MAPBIOMAS'], template= mask)
+  r_class <- st_rasterize(vec_i['C7_MAPBIOM'], template= mask)
   
   ## exportar
-  write_stars(r_cagb, paste0('./output/cagb/', 'tile_', i, '_cagb.tif'), type="Float32", drive="GTiff")
+  #write_stars(r_cagb, paste0('/output/cagb/', 'tile_', i, '_cagb.tif'), type="Float32", drive="GTiff")
   #write_stars(r_cbgb, paste0('./output/cbgb/', 'tile_', i, '_cbgb.tif'), type="Float32", drive="GTiff")
-  write_stars(r_cdw, paste0('./output/cdw/', 'tile_', i, '_cdw.tif'), type="Float32", drive="GTiff")
-  write_stars(r_clitter, paste0('./output/clitter/', 'tile_', i, '_clitter.tif'), type="Float32", drive="GTiff")
-  write_stars(r_cagb, paste0('./output/total/', 'tile_', i, '_total.tif'), type="Float32", drive="GTiff")
-  write_stars(r_class, paste0('./output/', 'tile_', i, '_qcnclass.tif'), type="Float32", drive="GTiff")
+  #write_stars(r_cdw, paste0('./output/cdw/', 'tile_', i, '_cdw.tif'), type="Float32", drive="GTiff")
+  #write_stars(r_clitter, paste0('./output/clitter/', 'tile_', i, '_clitter.tif'), type="Float32", drive="GTiff")
+  #write_stars(r_cagb, paste0('./output/total/', 'tile_', i, '_total.tif'), type="Float32", drive="GTiff")
+  #write_stars(r_class, paste0('./output/', 'tile_', i, '_c7_qcnclass.tif'), type="Float32", drive="GTiff")
+  
+  write_stars(r_cagb, paste0('C:/Users/edriano.souza/GitHub/2022_2_QCN_rectify_v2/data/shp/biomes_v0/output/cagb/', 'tile_', i, '_cagb.tif'), type="Float32", drive="GTiff")
+  write_stars(r_cbgb, paste0('C:/Users/edriano.souza/GitHub/2022_2_QCN_rectify_v2/data/shp/biomes_v0/output/cbgb/', 'tile_', i, '_cbgb.tif'), type="Float32", drive="GTiff")
+  write_stars(r_cdw, paste0('C:/Users/edriano.souza/GitHub/2022_2_QCN_rectify_v2/data/shp/biomes_v0/output/cdw/', 'tile_', i, '_cdw.tif'), type="Float32", drive="GTiff")
+  write_stars(r_clitter, paste0('C:/Users/edriano.souza/GitHub/2022_2_QCN_rectify_v2/data/shp/biomes_v0/output/clitter/', 'tile_', i, '_clitter.tif'), type="Float32", drive="GTiff")
+  write_stars(r_cagb, paste0('C:/Users/edriano.souza/GitHub/2022_2_QCN_rectify_v2/data/shp/biomes_v0/output/total/', 'tile_', i, '_total.tif'), type="Float32", drive="GTiff")
+  write_stars(r_class, paste0('C:/Users/edriano.souza/GitHub/2022_2_QCN_rectify_v2/data/shp/biomes_v0/output/', 'tile_', i, '_c7_qcnclass.tif'), type="Float32", drive="GTiff")
+  
+  
   ## clean cache
   rm(tile_i, vec_i, mask, r_cagb, r_cbgb, r_cdw, r_clitter, r_total, r_class)
   gc()

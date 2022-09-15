ee3d8ef1b59d11df1abe11969b77f43a0e493b36
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2022-09-14 23:15:53 -0300

    Update 2_0_2_GenMetadata.R

--- 4._QCN/2_0_2_GenMetadata.R
+++ 4._QCN/2_0_2_GenMetadata.R
@@ -2,7 +2,7 @@
 ## edriano.souza@ipam.org.br or dhemerson.costa@ipam.org.br
 
 ## read files
-filename <- tools::file_path_sans_ext(basename(list.files('./output/')))
+filename <- tools::file_path_sans_ext(basename(list.files('C:/Users/edriano.souza/GitHub/2022_2_QCN_rectify_v2/data/shp/biomes_v0/output')))
 
 ## parse info
 tile_n <- sapply(strsplit(filename, split='_', fixed=TRUE), function(x) (x[2]))
@@ -12,4 +12,4 @@ product <- sapply(strsplit(filename, split='_', fixed=TRUE), function(x) (x[3]))
 metadata <- cbind(filename, tile_n, product)
 
 ## export
-write.csv(metadata, './metadata.csv', row.names=FALSE)
+write.csv(metadata, 'C:/Users/edriano.souza/GitHub/2022_2_QCN_rectify_v2/data/shp/biomes_v0/output/metadata.csv', row.names=FALSE)

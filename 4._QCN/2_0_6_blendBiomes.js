bd6c554e77530d322f145d88021b17df3df7c3fa
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2022-09-29 00:51:00 -0300

    Update 2_0_6_blendBiomes.js

--- 4._QCN/2_0_6_blendBiomes.js
+++ 4._QCN/2_0_6_blendBiomes.js
@@ -1,5 +1,5 @@
 // standardize assets per biome
-// edriano.souza@ipam.org.br; dhemerson.costa@ipam.org.br ; wallace.silva@ipam.org.br
+// edriano.souza@ipam.org.br; dhemerson.costa@ipam.org.br ; 
 
 // define output directory
 var dir_out = 'projects/mapbiomas-workspace/SEEG/2022/QCN/QCN_30m_BR_v2_0_1/';

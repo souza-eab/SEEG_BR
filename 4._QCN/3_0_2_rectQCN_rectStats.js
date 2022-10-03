efda3f5770a14136bd6ced1cbf32f6b2b312ce4d
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2022-10-03 15:32:53 -0300

    Update 3_0_2_rectQCN_rectStats.js

--- 4._QCN/3_0_2_rectQCN_rectStats.js
+++ 4._QCN/3_0_2_rectQCN_rectStats.js
@@ -2,10 +2,9 @@
 // any issue or bug, please write to edriano.souza@ipam.org.br and/or dhemerson.costa@ipam.org.br  
 
 // define years to be processed 
-var list_mapb_years = [1985//, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997,
-                       //1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
-                       //2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019,
-                       //2020
+var list_mapb_years = [1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997,
+                       1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
+                       2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019,2020, 2021
                        ];
                        
 // define biomes to be processed
@@ -19,34 +18,36 @@ var list_biomes = [//1, // amazonia
                        
 // define QCN classes to extract statistics 
 var list_qcn_classes = [3,  // Forest formation
-                        //4,  // Savanna formation 
-                        //5,  // Mangrove
-                        //12, // Grassland 
-                        ];
+                        4,  // Savanna formation 
+                        5,  // Mangrove
+                        11, // Wetland
+                        12, // Grassland 
+                        49, // Wooded Restinga
+                        50]; // Shrubby Restinga //*BZ
 
-// define mapbiomas colelction 6.0 reclassification matrix
-var raw_mapbiomas  = [3, 4, 5, 9, 11, 12, 13, 15, 20, 21, 23, 24, 25, 29, 30, 31, 32, 33, 39, 40, 41, 46, 47, 48, 49];   
-var reclass_vector = [3, 4, 5, 0, 12, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  5];   
+// define mapbiomas colelction 7.0 reclassification matrix
+var raw_mapbiomas  = [3, 4, 5, 9, 11, 12, 13, 15, 20, 21, 23, 24, 25, 29, 30, 31, 32, 33, 36, 39, 40, 41, 46, 47, 48, 49, 50, 62];   //*BZ
+var reclass_vector = [3, 4, 5, 0, 11, 12,  13,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 0,  0,  0,  0,  0,  0, 49, 50, 0];   //*BZ
 
 // total c - raw qcn
-var raw_qcn = ee.ImageCollection('projects/mapbiomas-workspace/SEEG/2021/QCN/QCN_30m_c')
+var raw_qcn = ee.ImageCollection('projects/mapbiomas-workspace/SEEG/2022/QCN/QCN_30m_BR_v2_0_1')
                 .mosaic()
                 .select(['total']);
 
 // total c - rectified qcn  
-var rec_qcn = ee.ImageCollection('projects/mapbiomas-workspace/SEEG/2021/QCN/QCN_30m_rect')
+var rec_qcn = ee.ImageCollection('projects/mapbiomas-workspace/SEEG/2022/QCN/QCN_30m_BR_v2_0_2_Rect')
                 .mosaic();
 
 // LCLU classes - qcn 
-var cls_qcn = ee.ImageCollection('projects/mapbiomas-workspace/SEEG/2021/QCN/QCN_30m_c')
+var cls_qcn = ee.ImageCollection('projects/mapbiomas-workspace/SEEG/2022/QCN/QCN_30m_BR_v2_0_1')
                 .mosaic()
                 .select(['qcnclass']);
 
 // LCLU classes - mapbiomas    
-var cls_mpb = ee.Image("projects/mapbiomas-workspace/public/collection6/mapbiomas_collection60_integration_v1");
+var cls_mpb = ee.Image("projects/mapbiomas-workspace/public/collection7/mapbiomas_collection70_integration_v2");
 
 // brazilian states
-var br_stt = ee.FeatureCollection('projects/mapbiomas-workspace/AUXILIAR/estados-2017');
+var br_stt = ee.FeatureCollection('projects/mapbiomas-workspace/AUXILIAR/estados-2016-raster');
 //var br_stt = br_stt.limit(3);
 
 // biomes
@@ -55,8 +56,8 @@ var biomes = ee.Image('projects/mapbiomas-workspace/AUXILIAR/biomas-2019-raster'
 // get color-ramp module
 var vis = {
     'min': 0,
-    'max': 49,
-    'palette': require('users/mapbiomas/modules:Palettes.js').get('classification6')
+    'max': 62,
+    'palette': require('users/mapbiomas/modules:Palettes.js').get('classification7')
 };
 
 // define empty recipe 
@@ -163,7 +164,7 @@ list_mapb_years.forEach(function(year_i) {
       // select c total
           discordance_ijk = rec_qcn_ij.updateMask(discordance_ijk);
           
-Map.addLayer(concordance_ijk.blend(discordance_ijk), vis, 'PABLO');
+//Map.addLayer(concordance_ijk.blend(discordance_ijk), vis, 'PABLO');
       
       // define function to compute disagreement mean
       var getDisagreementMean = function (feature) {
@@ -218,3 +219,11 @@ Export.table.toDrive({
   folder: 'EXPORT',
   fileFormat: 'CSV'
 });
+
+
+var Mapp = require('users/joaovsiqueira1/packages:Mapp.js');
+var ColorRamp = require('users/joaovsiqueira1/packages:ColorRamp.js');
+
+Map.setOptions({ 'styles': { 'Dark': Mapp.getStyle('Dark'), 'Dark2':Mapp.getStyle('Dark2'), 'Aubergine':Mapp.getStyle('Aubergine'), 'Silver':Mapp.getStyle('Silver'), 'Night':Mapp.getStyle('Night'), } });
+// The most recent zoom is the one the view will have.
+Map.setZoom(6)

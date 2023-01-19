2e4a136ec7c71f04bc9d26a14386cd101d979313
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2023-01-19 17:06:15 -0300

    Update 2.0_Stabilized_cover.js

--- 1._Spatial_analyses/2.0_Stabilized_cover.js
+++ 1._Spatial_analyses/2.0_Stabilized_cover.js
@@ -50,8 +50,15 @@ print("bandas", bandNames);
 // 1985 -> 2021  //* Verificar o somatório das Bandas conforme o periodo
 //////// Calculate the frequency (number of years) in which each i_pixel was a determined class_n
 // General rule (ratio of the total number of years of the considered period)
+//var exp = '100*((b(0)+b(1)+b(2)+b(3)+b(4)+b(5)+b(6)+b(7)+b(8)+b(9)+b(10)+b(11)+b(12)+b(13)+b(14)+b(15)' +
+//    '+b(16)+b(17)+b(18)+b(19)+b(20)+b(21)+b(22)+b(23)+b(24)+b(25)+b(26)+b(27)+b(28)+b(29)+b(30)+b(31)+b(32)+b(33)+b(34)+b(35)+b(36))/37)'; // Each collection adds one more year and it is important to check the period of the Mapbiomas collection being used
+
+
+// 1989 -> 2021
+//////// Calculate the frequency (number of years) in which each i_pixel was a determined class_n
+// General rule (ratio of the total number of years of the considered period)
 var exp = '100*((b(0)+b(1)+b(2)+b(3)+b(4)+b(5)+b(6)+b(7)+b(8)+b(9)+b(10)+b(11)+b(12)+b(13)+b(14)+b(15)' +
-    '+b(16)+b(17)+b(18)+b(19)+b(20)+b(21)+b(22)+b(23)+b(24)+b(25)+b(26)+b(27)+b(28)+b(29)+b(30)+b(31)+b(32)+b(33)+b(34)+b(35)+b(36))/37)'; // Each collection adds one more year and it is important to check the period of the Mapbiomas collection being used
+    '+b(16)+b(17)+b(18)+b(19)+b(20)+b(21)+b(22)+b(23)+b(24)+b(25)+b(26)+b(27)+b(28)+b(29)+b(30)+b(31)+b(32)/33)'; // Each collection adds one more year and it is important to check the period of the Mapbiomas collection being used
 
 // Get frequency of each class
 var florFreq = mapbiomas.eq(3).expression(exp);                     //  Forest Formation

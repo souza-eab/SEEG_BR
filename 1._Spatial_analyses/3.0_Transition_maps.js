7ce49ba1e8a5b090f481f0b54241d4769588cdf9
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2023-01-20 17:24:59 -0300

    Update 3.0_Transition_maps.js

--- 1._Spatial_analyses/3.0_Transition_maps.js
+++ 1._Spatial_analyses/3.0_Transition_maps.js
@@ -28,7 +28,7 @@ var regions = ee.FeatureCollection(assetRegions);
 var years = ['1985','1986','1987','1988','1989', '1990','1991','1992','1993','1994','1995','1996','1997','1998','1999','2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019', '2020','2021'];
 
 // Load the assets from the previous step 'Stabilized_cover'
-var cover = ee.ImageCollection('projects/ee-seeg-brazil/assets/collection_10/v1/2_1_Mask_stable').aside(print);
+var cover = ee.ImageCollection('projects/mapbiomas-workspace/SEEG/2023/c10/2_0_Mask_stable').aside(print);
 
 // Loop to do the arithmetic of bands with all pairs of years, multiplying year 1 by 10000
 years.forEach(function(year){
